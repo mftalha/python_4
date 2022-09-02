8ef004bc2092a5d31f77ba6c476aa24fbaf139ca
Talha Satır <talha.satir@vinya.com.tr>
2022-09-02 10:31:16 +0300

    commit 5 düzeltme

--- send_python_1.py
+++ send_python_1.py
@@ -2,4 +2,4 @@ print("github için ilk hal-1");
 print("github için güncel hal-2");
 print("github için güncel hal-3");
 print("github için güncel hal-4");
-print("github için güncel hal-5")
+print("github için güncel hal-5");
